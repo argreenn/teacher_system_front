986fe01c0344b00ee603811f0830ef25d4713c4d
argree <911192479@qq.com>
2025-03-12 19:20:55 +0800

    教师管理系统,github获取路径矫正

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/teacher_system_front/',
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+    assetsDir: './'
+  }
 })
